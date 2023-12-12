2e2cfca9218f5faa6f7ff62f51aad23c59cf1e89
Vinícius Inácio <sobral.321@hotmail.com>
2023-12-11 22:03:25 -0300

    added main menu function and variable main

---
+++ functions/showMainMenu.h
@@ -0,0 +1,13 @@
+extern void showMainMenu()
+{
+    printf("--- Bem vindo ao SGDB ITP ---\n\n");
+    printf("O que deseja fazer:\n");
+    printf("1 - Criar nova tabela\n");
+    printf("2 - Listar tabelas existentes\n");
+    printf("3 - Criar nova linha em uma tabela existente\n");
+    printf("4 - Listar todos os dados de uma tabela\n");
+    printf("5 - Pesquisar um valor em uma tabela\n");
+    printf("6 - Apagar uma linha (tupla)\n");
+    printf("7 - Apagar uma tabela existente\n\n");
+    printf("Sua escolha: ");
+}
